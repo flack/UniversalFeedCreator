8e5f3826946cbd486424d2cf6f4f0866eb71898f
Andreas Gohr <andi@splitbrain.org>
2017-05-16 18:01:54 +0200

    use the most recent phpunit available for the current PHP release
    Composer will install the most modern phpunit which's requirements
    (namely the PHP version) can be satisfied by the current environment.
    
    The travis config was adjusted to make use of that phpunit release and
    to test all currently available PHP versions.
    
    The bootstrap file will alias the namespaced phpunit classes to their
    non-namespaced names.

--- test/rootfile.php
+++ test/rootfile.php
@@ -1,3 +1,18 @@
 <?php
+/**
+ * PHP Unit bootstrapping
+ *
+ * the used phpunit version depends on the available PHP version, newer versions are namespaced
+ * whiler older ones are not.
+ */
+
 require dirname(__DIR__) . '/vendor/autoload.php';
-?>
+
+if (
+    !class_exists('\PHPUnit_Framework_TestCase')
+    && class_exists('\PHPUnit\Framework\TestCase')
+) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+    class_alias('\PHPUnit\Framework\Constraint\IsEqual', '\PHPUnit_Framework_Constraint_IsEqual');
+    class_alias('\PHPUnit\Util\InvalidArgumentHelper', '\PHPUnit_Util_InvalidArgumentHelper');
+}
