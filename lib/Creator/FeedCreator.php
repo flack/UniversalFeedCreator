3abf36ef9d3a3b31a3c330caea7d5176f0396301
flack <flack@contentcontrol-berlin.de>
2024-01-23 16:59:31 +0100

    Remove unused property

--- lib/Creator/FeedCreator.php
+++ lib/Creator/FeedCreator.php
@@ -53,7 +53,6 @@ abstract class FeedCreator extends HtmlDescribable
     protected $encoding = "UTF-8"; //"ISO-8859-1";
 
     protected $_timeout;  # lib/Creator/FeedCreator.php  line 238
-    protected $truncSize; # lib/Creator/PIECreator01.php line 29
 
     /**
      * Any additional elements to include as an associated array. All $key => $value pairs
